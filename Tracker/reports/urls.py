c873f8c7119205bd34e8c078cdfcced296f3ab60
A.Talha Timur <93510585+ATalhaTimur@users.noreply.github.com>
2024-11-30 03:38:18 +0300

    all qualifications are done

--- Tracker/reports/urls.py
+++ Tracker/reports/urls.py
@@ -4,10 +4,10 @@ from .api_views import ReportsAPIView
 from .views import ReportsPersonnelView, ReportsManagerView
 
 urlpatterns = [
-    #front end views
+    # front-end views
     path('my/', ReportsPersonnelView.as_view(), name='personnel-reports'),
-    path('', ReportsManagerView.as_view(), name='reports-manager'),
+    path('manager/', ReportsManagerView.as_view(), name='reports-manager'),
 
-    #backend API
-    path('api/reports/', ReportsAPIView.as_view(), name='reports-api-v2'),  # Yeni backend API
+    # back-end API
+    path('api/reports/', ReportsAPIView.as_view(), name='reports-api-v2'),
 ]
--- Tracker/reports/views.py
+++ Tracker/reports/views.py
@@ -2,11 +2,13 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import WorkReport
 
+
 class ReportsPersonnelView(LoginRequiredMixin, TemplateView):
     template_name = "reports/reports_personnel.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Sadece kendi raporlarını filtrele
         context['reports'] = WorkReport.objects.filter(user=self.request.user).order_by('-month')
         return context
 
@@ -16,6 +18,7 @@ class ReportsManagerView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if self.request.user.role == "manager":
-            context['reports'] = WorkReport.objects.all().order_by('-month')
+        # Yönetici tüm raporları görebilir
+        reports = WorkReport.objects.select_related('user').all().order_by('-month')
+        context['reports'] = reports
         return context
